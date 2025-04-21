4a65087fd805846d49eba0fc228433bc104543bf
Rohit Verma <rohitve012@gmail.com>
2025-04-22 01:17:22 +0530

    `Update .env file and .gitignore file, modify db.js to remove MongoDB connection string`

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // For Local MongoDB
-        await mongoose.connect('mongodb://localhost:27017/smartspend');
+        await mongoose.connect("");
         
-        // For MongoDB Atlas (Uncomment & replace credentials)
-        // await mongoose.connect('mongodb+srv://deadlock:asrasr123@cluster0.mnhdnpb.mongodb.net/smartspend?retryWrites=true&w=majority');
         
         console.log("✅ MongoDB Connected!");
     } catch (error) {
